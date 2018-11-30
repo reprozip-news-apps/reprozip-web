b6390cbeeff0627c0e43894b75783f6f4604dfb1
Brian Hoffman <brianjhoffman@gmail.com>
2018-11-30 10:09:10 -0500

    make sure the unpacked site gets up and running before launching the recorder

--- scripts/record-rpz.sh
+++ scripts/record-rpz.sh
@@ -10,6 +10,7 @@ if [ ! $1 ] || [ ! $2 ]; then
     exit 0
 fi
 
+TARGET_HOST=http://localhost
 RPZ_FILE="$CWD/$1"
 PORT=$2
 NAME=${RPZ_FILE%%.*}
@@ -20,6 +21,17 @@ UNPACK_DIR=$TMP_DIR/$NAME
 if [ ! -e $RPZ_FILE ]; then
    echo "Can't find: $RPZ_FILE"
    exit 0
+fi
+
+if lsof -Pi :$PORT -sTCP:LISTEN -t >/dev/null ; then
+    echo "There is already something running on port $PORT!"
+    echo "Hint: docker ps"
+    exit 0
+fi
+
+if lsof -Pi :8080 -sTCP:LISTEN -t >/dev/null ; then
+    echo "FAILURE: The wayback server needs access to port 8080"
+    exit 0
 fi
 
 if [ ! -d $TMP_DIR ]; then
@@ -35,12 +47,18 @@ fi
 
 reprounzip docker run --detach --expose-port $PORT:$PORT $UNPACK_DIR
 
+while ! curl -I $TARGET_HOST:$PORT | grep -m1 'HTTP/1.1 200 OK'; do
+    echo "Waiting for 200 response from the target"
+    sleep 5
+done
+
 cd $TMP_DIR
 if [ ! -d collections/$NAME ]; then
     wb-manager init $NAME
 fi
 
 wayback --record --live -a --auto-interval 5 &> wayback.out &
+WB_PID=$!
 cd $CWD
 
 while ! grep -m1 'Starting Gevent Server on 8080' < $TMP_DIR/wayback.out; do
@@ -52,6 +70,7 @@ export TARGET_URL="http://localhost:$PORT"
 export COLLECTION_NAME=$NAME
 export PYWB_URL="http://localhost:8080"
 
+trap "{ kill $WB_PID; echo 'Wayback server did not shut down properly'; rm -rf $UNPACK_DIR; exit 1; }" SIGINT SIGTERM
 python3 recorder.py
 
 while ! grep -m1 'Auto-Indexing\.\.\.' < $TMP_DIR/wayback.out; do
@@ -59,7 +78,7 @@ while ! grep -m1 'Auto-Indexing\.\.\.' < $TMP_DIR/wayback.out; do
     sleep 1
 done
 
-kill %1
+kill $WB_PID
 LATEST_WARC=$(ls $TMP_DIR/collections/$NAME/archive/*.warc.gz | sort -V | tail -n 1)
 tar -r -f $RPZ_FILE -C ${LATEST_WARC%/*} ${LATEST_WARC##*/}
 echo "Done Recording:"
