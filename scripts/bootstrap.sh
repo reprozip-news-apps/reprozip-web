00cf5f9221b30a66f424c14f30fa5fca37149563
Brian Hoffman <brianjhoffman@gmail.com>
2019-03-05 09:23:26 -0500

    remove old scripts

--- scripts/bootstrap.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export PYPPETEER_CHROMIUM_REVISION=610995
-python3 bootstrap.py
--- scripts/pack-hello-mars.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname $0)/../example/hello-mars"
-
-docker build -t savingdatajournalism/hello-mars .
-
-docker run --rm --cap-add=SYS_PTRACE --name packing-hello-mars -v $(pwd):/hello-mars savingdatajournalism/hello-mars /bin/bash -c "cd /hello-mars && reprozip pack hello-mars-$(date "+%Y%M%dT%H%M%S")"
-
